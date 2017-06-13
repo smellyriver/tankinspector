6267a04217608ab7c6b70eb37de5824ad9bd2998
hillin <hillinsilence@gmail.com>
2017-06-13 10:41:12 +0800

    add dummy script classes for new battle booster comsumable items introduced in 9.19

---
+++ TankInspector/Modeling/Tank/AdditiveBattleBoosterScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smellyriver.TankInspector.Modeling
+{
+	internal class AdditiveBattleBoosterScript : ConsumableScript
+	{
+		public AdditiveBattleBoosterScript(Database database)
+			: base(database)
+		{
+		}
+
+		public override string[] EffectiveDomains => new string[0];
+		public override void Execute(ModificationContext context, object args)
+		{
+			// todo
+		}
+	}
+}
---
+++ TankInspector/Modeling/Tank/FactorSkillBattleBoosterScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smellyriver.TankInspector.Modeling
+{
+	internal class FactorSkillBattleBoosterScript : ConsumableScript
+	{
+		public FactorSkillBattleBoosterScript(Database database)
+			: base(database)
+		{
+		}
+
+		public override string[] EffectiveDomains => new string[0];
+		public override void Execute(ModificationContext context, object args)
+		{
+			// todo
+		}
+	}
+}
